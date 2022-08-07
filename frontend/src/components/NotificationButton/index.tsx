4276f1100c3275e2c5f9b90c37fb36a21cbbbdcf
Roberto Sistemas <roberto140172@gmail.com>
2022-08-07 15:44:57 -0300

    Primeiro Componente

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,17 @@
+
+import icon from "../../assets/img/Notification-icon.svg"
+import "./styles.css"
+function NotificationButton() {
+    return (
+        <>
+            <h1>Roberto Sistemas</h1>
+            <div className="botao-red-btn">
+                <img src={icon} alt="Notificar" />
+
+            </div>
+
+        </>
+    )
+}
+
+export default NotificationButton
